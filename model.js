3aa65d3461f85e42bcc0a122fc89bb2ae658ac2d
fhacquesmini <florestan.hacques-minier@etudiant.univ-rennes1.fr>
2022-03-02 18:48:40 +0100

    Model Q6

--- model.js
+++ model.js
@@ -1,3 +1,27 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing() {
+    this.shapeArray = new Array()
+}
+
+function Shape(x, y, color, thickness) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.thickness = thickness
+
+}
+
+function Rectangle(x, y, color, thickness, height, width) {
+    Shape.call(this, x, y, color, thickness)
+    this.width = width
+    this. heigth = height
+}
+
+function Line(x, y, color, thickness, x2, y2) {
+    Shape.call(this, x, y, color, thickness)
+    this.x2 = x2
+    this.y2 = y2
+}
